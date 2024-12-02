cb0e6c57b84cbcf64f546a278997baaf0cf9e435
Andrii Prykhodko <andriichello@gmail.com>
2024-12-02 15:24:26 +0200

    Check currently logged in user and logout if username is different

--- resources/puppeteer/work.js
+++ resources/puppeteer/work.js
@@ -2,6 +2,12 @@ import puppeteer from 'puppeteer';
 import PuppeteerExtra from "puppeteer-extra";
 import Stealth from "puppeteer-extra-plugin-stealth";
 
+const args = process.argv.slice(2);
+
+const username = args[0];
+const password = args[1];
+const filePath = args[2];
+
 PuppeteerExtra.use(Stealth());
 
 // Launch the browser and open a new blank page
@@ -19,12 +25,29 @@ await page.setViewport({width: 1080, height: 1024});
 // Wait until everything loads.
 await page.waitForNetworkIdle();
 
-let profileButton = await page.$('[data-testid="profile-button"]');
+let profileButton = await page.$('[data-testid="profile-button"], [data-testid="accounts-profile-button"]');
 
 if (profileButton) {
     console.log('is logged in');
-    profileButton = null;
-} else {
+    await profileButton.click();
+
+    const loggedInAs = await page.$eval(
+        'div.popover div.ml-3.mr-2.py-2.text-sm.text-token-text-secondary',
+        (element) => element.textContent.trim()
+    );
+
+    console.log(loggedInAs);
+
+    if (loggedInAs !== username) {
+        profileButton = null;
+        await page.goto('https://chatgpt.com/auth/logout');
+
+        // Wait until everything loads.
+        await page.waitForNetworkIdle();
+    }
+}
+
+if (!profileButton){
     console.log('is not logged in');
 
     let logIn = await page.$('[data-testid="welcome-login-button"]')
@@ -39,14 +62,14 @@ if (profileButton) {
     console.log('waiting for an email input');
     await page.waitForSelector(
         '#email-input, [name="username"], [type="email"]',
-        { timeout: 5000 },
+        { timeout: 10000 },
     )
 
     console.log('looking for an email input');
     let emailInput = await page.$('#email-input, [name="username"], [type="email"]');
 
     if (emailInput) {
-        await emailInput.type('mealtofeel+one@gmail.com');
+        await emailInput.type(username);
         emailInput = null;
     }
 
@@ -76,7 +99,7 @@ if (profileButton) {
 
     if (passwordInput) {
         console.log('there is a password input');
-        await passwordInput.type('xzymaNjaVeV2GaP');
+        await passwordInput.type(password);
         passwordInput = null;
     }
 
@@ -154,7 +177,7 @@ if (fileInput) {
         console.log('The file input is disabled');
     }
 
-    await fileInput.uploadFile('/var/www/meal-to-feel-api/resources/puppeteer/screenshots/fruit-salad.jpg');
+    await fileInput.uploadFile(filePath);
 } else {
     console.log('there is no file input');
 }
@@ -176,68 +199,68 @@ const attachDisabled = await page.evaluate(() => {
 })
 
 if (attachDisabled) {
-    // await page.close();
-    // await browser.disconnect();
-    //
-    // // Postponed (Attaching files is not available)
-    // process.exit(-1);
+    await page.close();
+    await browser.disconnect();
+
+    // Postponed (Attaching files is not available)
+    process.exit(-1);
 }
+//
+// let textarea = await page.$('textarea');
+//
+// if (textarea) {
+//     const prompt = 'Here is a photo of the dish. Please estimate calories, nutrients.'
+//         + ' Please respond in JSON format (weight in grams): {"meal": "Name the meal","description":"Describe if meal is healthy or not.", "error": "Describe the error (might be no food on photo).","ingredients":[{"name":"Ingredient","serving_size":"1 medium sized","weight":130.5,"calories":62,"carbohydrates":15.4,"fiber":3.1,"sugar":12.2,"protein":1.2,"fat":0.2}],"total":{"weight":130.5,"calories":62,"carbohydrates":15.4,"fiber":3.1,"sugar":12.2,"protein":1.2,"fat":0.2}}.'
+//         + ' If there is no photo then for now just return the example JSON. Please respond in language with code: uk.'
+//         // + ' Please respond in JSON format (weight in grams): {"meal": "Name the meal","description":"Describe if meal is healthy or not.","ingredients":[{"name":"Ingredient","serving_size":"1 medium sized","weight":130.5,"calories":62,"carbohydrates":15.4,"fiber":3.1,"sugar":12.2,"protein":1.2,"fat":0.2}],"total":{"weight":130.5,"calories":62,"carbohydrates":15.4,"fiber":3.1,"sugar":12.2,"protein":1.2,"fat":0.2}}. For now just return the JSON example I provided.'
+//         + '\n';
+//
+//     await textarea.type(prompt);
+// }
+//
+// try {
+//     console.log('Waiting for the stop streaming button to disappear...');
+//     await page.waitForFunction(
+//         (sel) => !document.querySelector(sel),
+//         { timeout: 60000 }, // Timeout in milliseconds
+//         'button[data-testid="stop-button"]'
+//     );
+//     console.log('Button disappeared');
+// } catch (error) {
+//     console.error('Button did not disappear within the timeout', error);
+// }
+//
+// try {
+//     await page.waitForNetworkIdle({ timeout: 10000 });
+// } catch (Error) {
+//     //
+// }
+//
+// const jsonElement = await page.$('code.hljs.language-json');
+//
+// if (jsonElement) {
+//     console.log('Found the JSON element');
+//
+//     // Get the inner text of the element
+//     const jsonString = await page.evaluate(element => element.innerText, jsonElement);
+//
+//     try {
+//         // Parse the JSON string into an object
+//         const jsonData = JSON.parse(jsonString);
+//         console.log('Parsed JSON:', jsonData);
+//     } catch (error) {
+//         console.error('Failed to parse JSON:', error);
+//
+//         await page.close();
+//         await browser.disconnect();
+//
+//         process.exit(-2);
+//     }
+// } else {
+//     console.log('JSON element not found');
+//
+//     process.exit(-3);
+// }
 
-let textarea = await page.$('textarea');
-
-if (textarea) {
-    const prompt = 'Here is a photo of the dish. Please estimate calories, nutrients.'
-        + ' Please respond in JSON format (weight in grams): {"meal": "Name the meal","description":"Describe if meal is healthy or not.", "error": "Describe the error (might be no food on photo).","ingredients":[{"name":"Ingredient","serving_size":"1 medium sized","weight":130.5,"calories":62,"carbohydrates":15.4,"fiber":3.1,"sugar":12.2,"protein":1.2,"fat":0.2}],"total":{"weight":130.5,"calories":62,"carbohydrates":15.4,"fiber":3.1,"sugar":12.2,"protein":1.2,"fat":0.2}}.'
-        + ' If there is no photo then for now just return the example JSON. Please respond in language with code: uk.'
-        // + ' Please respond in JSON format (weight in grams): {"meal": "Name the meal","description":"Describe if meal is healthy or not.","ingredients":[{"name":"Ingredient","serving_size":"1 medium sized","weight":130.5,"calories":62,"carbohydrates":15.4,"fiber":3.1,"sugar":12.2,"protein":1.2,"fat":0.2}],"total":{"weight":130.5,"calories":62,"carbohydrates":15.4,"fiber":3.1,"sugar":12.2,"protein":1.2,"fat":0.2}}. For now just return the JSON example I provided.'
-        + '\n';
-
-    await textarea.type(prompt);
-}
-
-try {
-    console.log('Waiting for the stop streaming button to disappear...');
-    await page.waitForFunction(
-        (sel) => !document.querySelector(sel),
-        { timeout: 60000 }, // Timeout in milliseconds
-        'button[data-testid="stop-button"]'
-    );
-    console.log('Button disappeared');
-} catch (error) {
-    console.error('Button did not disappear within the timeout', error);
-}
-
-try {
-    await page.waitForNetworkIdle({ timeout: 10000 });
-} catch (Error) {
-    //
-}
-
-const jsonElement = await page.$('code.hljs.language-json');
-
-if (jsonElement) {
-    console.log('Found the JSON element');
-
-    // Get the inner text of the element
-    const jsonString = await page.evaluate(element => element.innerText, jsonElement);
-
-    try {
-        // Parse the JSON string into an object
-        const jsonData = JSON.parse(jsonString);
-        console.log('Parsed JSON:', jsonData);
-    } catch (error) {
-        console.error('Failed to parse JSON:', error);
-
-        await page.close();
-        await browser.disconnect();
-
-        process.exit(-2);
-    }
-} else {
-    console.log('JSON element not found');
-
-    process.exit(-3);
-}
-
-await page.close();
+// await page.close();
 await browser.disconnect();
